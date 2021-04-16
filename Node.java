3eb6502c01eec94f324a55cc43a4863a5df0dcb7
siskanurcahyaniimamah <75078269+siskanurcahyaniimamah@users.noreply.github.com>
2021-04-16 09:42:32 -0700

    week 05

---
+++ Node.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author siska nur cahyani
+ */
+public class Node {
+    public int id;
+    public String data;
+    public Node leftChild;
+    public Node rightChild;
+    
+    public void displayNode() {
+        System.out.print("{ " + id + ", " + data + " } ");
+    }
+}
